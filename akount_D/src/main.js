42d1eabfe371d4d95aaa7913c7ce8c607c6d861b
JS <jisu0259@naver.com>
2024-06-11 14:03:52 +0900

    awesome font 구현

--- akount_D/src/main.js
+++ akount_D/src/main.js
@@ -8,7 +8,18 @@ import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.css";
 
+// FontAwesome 설정
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse, faTree, faDog } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// 아이콘을 라이브러리에 추가
+library.add(faHouse, faTree, faDog);
+
 const app = createApp(App);
+
+// FontAwesome 컴포넌트를 글로벌로 등록
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
--- akount_D/src/stores/content.js
+++ akount_D/src/stores/content.js
@@ -1,12 +1,10 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useContentStore = defineStore('content', () => {
-    const BASEURI = '/api/content'
-    const state=reactive({content: []
-  })
-    const count = ref(0)
-    
+export const useContentStore = defineStore("content", () => {
+    const BASEURI = "/api/content";
+    const state = reactive({ content: [] });
+    const count = ref(0);
 
-  return {  }
-})
+    return {};
+});
