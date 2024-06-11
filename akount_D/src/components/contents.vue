81d81c1147739bcf52f7bafe6322f74e526dadf8
JS <jisu0259@naver.com>
2024-06-11 17:53:09 +0900

    Write 컴포넌트 초안

---
+++ akount_D/src/components/contents.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRouter } from "vue-router";
+import { useContentStore } from "@/stores/content.js";
+import { onMounted } from "vue";
+
+defineProps({
+    contents: { Type: Object, required: true },
+});
+
+const router = useRouter();
+const contentStore = useContentStore();
+onMounted(() => {
+    console.log(`컴포넌트가 마운트 됐습니다.`);
+});
+</script>
+
+<template>
+    <li>
+        <span>{{ contents.memo }}</span>
+    </li>
+</template>
--- akount_D/src/stores/content.js
+++ akount_D/src/stores/content.js
@@ -1,10 +1,25 @@
-import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
+import axios from "axios";
 
-export const useContentStore = defineStore("content", () => {
+export const useContentStore = defineStore("contentList", () => {
     const BASEURI = "/api/content";
-    const state = reactive({ content: [] });
-    const count = ref(0);
+    const state = reactive({ contentList: [] });
 
-    return {};
+    const fetchContent = async () => {
+        try {
+            const response = await axios.get(BASEURI);
+            if (response.status === 200) {
+                state.contentList = response.data; // 변수명 일치하도록 수정
+            } else {
+                alert("데이터 조회 실패");
+            }
+        } catch (error) {
+            alert("에러발생:" + error);
+        }
+    };
+
+    const contentList = computed(() => state.contentList);
+
+    return { state, contentList, fetchContent };
 });
