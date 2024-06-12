6c79df2d0be26a74650f6fdde13a10ac979636a0
Donghee Kim <eehd1127@naver.com>
2024-06-12 13:49:01 +0900

    Profile.vue 컴포넌트 db 구현(get/post), content.js 코드 일부 수정, saveProfile 함수 추가, setProfile 함수 삭제

--- akount_D/src/pages/Profile.vue
+++ akount_D/src/pages/Profile.vue
@@ -1,110 +1,55 @@
 <template>
-
-    <div
-        class="container d-flex justify-content-center align-items-start min-vh-100 py-5"
-    >
+    <div class="container d-flex justify-content-center align-items-start min-vh-100 py-5">
         <div class="row col-12 col-md-8 col-lg-6">
-            <h2 class="text-center mb-4">마이페이지</h2>
+            <h2 class="text-center mb-4"> 마이페이지 </h2>
             <form>
-                <div
-                    class="mb-4 d-flex align-items-center justify-content-center"
-                >
+                <div class="mb-4 d-flex align-items-center justify-content-center">
                     <div class="d-flex align-items-center">
-                        <img
-                            src="../assets/profile.png"
-                            class="rounded-circle me-3"
-                            width="100"
-                            height="100"
-                        />
-                        <div>
-                            <div class="fw-bold">
-
-                                {{ profileStore.profile.nickname }} 님
-                            </div>
-                            <div class="text-muted">
-                                @ {{ profileStore.profile.account_id }}
-
-                            </div>
+                        <img src="../assets/profile.png" class="rounded-circle me-3" width="100" height="100"> 
+                        <div>                   
+                            <div class="fw-bold">{{ profileStore.profile.nickname }} 님</div>
+                            <div class="text-muted">@ {{ profileStore.profile.account_id }}</div> 
                         </div>
-                    </div>
+                    </div>                   
                 </div>
                 <div class="mt-3 mb-3">
-                    <label
-                        for="nickname"
-                        style="font-weight: bold"
-                        class="form-label"
-                        >별명</label
-                    >
-                    <input
-                        type="text"
-                        class="form-control"
-                        :placeholder="profileStore.profile.nickname"
-                        v-model="profileStore.profile.nickname"
-                    />
+                    <label for="nickname" style="font-weight: bold" class="form-label">별명</label>
+                    <input type="text" class="form-control" :placeholder="profileStore.profile.nickname" v-model="profileStore.profile.nickname">
                 </div>
                 <div class="mt-3 mb-3">
-                    <label
-                        for="email"
-                        style="font-weight: bold"
-                        class="form-label"
-                        >이메일</label
-                    >
-                    <input
-                        type="text"
-                        class="form-control"
-                        :placeholder="profileStore.profile.email"
-                        v-model="profileStore.profile.email"
-                    />
+                    <label for="email" style="font-weight: bold" class="form-label">이메일</label>
+                    <input type="text" class="form-control" :placeholder="profileStore.profile.email" v-model="profileStore.profile.email">
                 </div>
                 <div class="mt-3 mb-3">
-                    <label
-                        for="phone_number"
-                        style="font-weight: bold"
-                        class="form-label"
-
-                        >전화번호</label
-                    >
-                    <input
-                        type="text"
-                        class="form-control"
-
-                        :placeholder="profileStore.profile.phone_number"
-                        v-model="profileStore.profile.phone_number"
-
-                    />
+                    <label for="phone_number" style="font-weight: bold" class="form-label">전화번호</label>
+                    <input type="text" class="form-control" :placeholder="profileStore.profile.phone_number" v-model="profileStore.profile.phone_number">
                 </div>
                 <div class="d-md-flex justify-content-md-end">
-                    <button
-                        type="button"
-                        class="btn btn-primary btn-block mt-3"
-
-                        @click="saveChanges"
-
-                    >
-                        변경 사항 저장
-                    </button>
+                    <button type="button" class="btn btn-primary btn-block mt-3" @click="saveChanges">변경 사항 저장</button>
                 </div>
             </form>
         </div>
     </div>
-</template>
-
-<script setup>
-import { useRoute } from "vue-router";
-
-import { onMounted } from "vue";
-import axios from "axios";
-import { useMainStore } from "@/stores/content.js";
-
-const currentRoute = useRoute();
-const profileStore = useMainStore();
-
-onMounted(() => {
-    profileStore.fetchProfile();
-
-});
-
-const saveChanges = () => {
-    profileStore.setProfile(profileStore.profile);
-};
-</script>
+  </template>
+  
+  
+  <script setup>
+  import { useRoute } from "vue-router";
+  import { onMounted } from 'vue';
+  import axios from 'axios';
+  import { useMainStore } from "@/stores/content.js";
+  
+  const currentRoute = useRoute();
+  const profileStore = useMainStore();
+  
+  onMounted(async() => {
+      await profileStore.fetchProfile();
+  });
+  
+  
+  
+  const saveChanges = async() => {
+    await profileStore.saveProfile();
+  }
+  
+  </script>
